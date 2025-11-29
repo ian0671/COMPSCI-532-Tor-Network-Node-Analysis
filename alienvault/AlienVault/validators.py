44409cb1741d7eba1a7db25ccd936bdd3615ed65
Pradnya Apte <114807672+Pradnya-Apte@users.noreply.github.com>
2025-11-29 02:00:35 -0500

    Delete alienvault directory

--- alienvault/AlienVault/validators.py
+++
@@ -1,3 +0,0 @@
-def validate_pulse_minimal(p: dict) -> bool:
-    required = ["id", "name", "modified"]
-    return all(k in p for k in required)
--- alienvault/function_app.py
+++
@@ -1,9 +0,0 @@
-import logging
-import azure.functions as func
-#from VirusTotalTimer import Virus_Total_Timer
-from AlienVault.AlienVaultTimer import AlienVault_OTX_Timer
-
-
-app = func.FunctionApp()
-#app.register_functions(Virus_Total_Timer)
-app.register_functions(AlienVault_OTX_Timer)
